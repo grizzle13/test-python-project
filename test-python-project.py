3bf37eee256fa594f74bf2d309d84f9bd8b11f11
Sam <sam.briggs07@gmail.com>
2023-01-03 16:26:40 -0800

    First Commit

---
+++ test-python-project.py
@@ -0,0 +1,13 @@
+import matplotlib
+import numpy
+import scipy
+import pandas
+
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+pandas.read_csv("/Users/briggssa/Repos/test-python-project/test-data.csv")
+
+# Install Python Flask
