01338ad18cec535a856dc34160e00ae30c22f6a7
Sam <sam.briggs07@gmail.com>
2023-01-08 15:17:43 -0800

    API Attempt
    -Attempted to implement API for file upload (Yields internal server error)
    -Updated requirements.txt to remove unnecessary dependencies

--- test-python-project.py
+++ test-python-project.py
@@ -1,3 +1,4 @@
+
 # %% Import packages
 
 import matplotlib as mpl
@@ -5,10 +6,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 import pandas as pd
+import os
+import flask
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
 import pathlib
+# from collections.abc import Sequence # Keeps throwing an error - seems to be associated with a change in Python 3.10+
 # import tkinter as tk           # This would open a dialog to select a file but
 # from tkinter import filedialog # it doesn't seem to work with the venv
 # file_path = filedialog.askopenfilename()
+
+# %% Start up API (I think...)
+upload_dir = '/data/uploads'
+allowed_extensions = {'xls', 'xlsx', 'csv'}
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = False
+app.config['UPLOAD_FOLDER'] = upload_dir
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1000 * 1000 # 10 MB file limit
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+app.run()
 
 # %% Import raw data
 data_dir = r'/Users/briggssa/Repos/test-python-project/'
@@ -50,5 +98,3 @@ plot_out_path += plot_file_type
 plt.grid(True)
 plt.savefig(plot_out_path)
 plt.show()
-
-# %%
